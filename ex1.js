296be9acacb0899242f02d6f2b88ca1a86bad006
Nanu <sureshharshtiwari@gmail.com>
2026-07-17 14:20:14 +0530

    created ex1.js file

---
+++ ex1.js
@@ -0,0 +1,16 @@
+// let a=10;
+// const b=25;
+// console.log("a=",a);
+// b=a++;
+// console.log("b=",b);
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+const sum=(a,b)=>a+b;
+console.log("Sum=",sum(10,20));
+console.log("Sum=",sum(1765239786,20));
+console.log("Sum=",sum(10,232532985));
+
