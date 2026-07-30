543b5e06d60ad13babc4d09942d214bc639bc427
Nanu <sureshharshtiwari@gmail.com>
2026-07-30 14:05:21 +0530

    let and conts

--- demo1.js
+++ demo1.js
@@ -1,28 +1,3 @@
-// Destructuring property
-
-// const num=[1,2,3,4,5];
-
-// const b=num[0];
-// const c=num[1];
-
-// const [a,b]=num;
-// console.log("b=",a);
-// console.log("c=",b);
-
-const student={
-    name:"Nanu",
-    age:87,
-    branch:"NANU"
-
-}
-// const name=student.name;
-// const age=student.age;
-// const branch=student.branch;
-
-const {name,age,branch}=student;
-
-console.log(name);
-console.log(age);
-console.log(branch);
-
-
+var a=10;
+var a=34;
+console.log("a=", a);
