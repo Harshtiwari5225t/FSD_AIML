72dd13b1f0096a6ee48999b459d128cfa0f5e2c0
Nanu <sureshharshtiwari@gmail.com>
2026-08-14 14:46:32 +0530

    dom

---
+++ React CDN App/index.js
@@ -0,0 +1,19 @@
+const Data=[
+    {image:"", Price:"3259/-"},
+    {image:"", Price:"3259/-"},
+    {image:"", Price:"3259/-"},
+    {image:"", Price:"3259/-"}
+]
+function Book(props){
+    const image=React.createElement("image",{src:props.image,with:"50px",height:"50px"});
+    const h2=React.createElement("h2",{color:"red"},"Price: "+props.price);
+    const child=React.createElement("div",{classname:"card"},[image,h2]);
+    return child;
+}
+
+const booklist=    React.createElement("div",{classname:"booklist"},Data.map((b)=>{
+        Book(b);
+    }));
+
+
+const parent=document.getElementById("root");
