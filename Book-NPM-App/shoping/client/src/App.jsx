c002d56c05d38e4201e495b9639ad8de6cdc15b7
Nanu <sureshharshtiwari@gmail.com>
2026-09-10 13:54:15 +0530

    react route

--- Book-NPM-App/shoping/client/src/App.jsx
+++ Book-NPM-App/shoping/client/src/App.jsx
@@ -1,10 +1,22 @@
 import UserLayout from "./pages/UserLayout"
 import "./App.css"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 const App = () => {
   return (
     <div>
-      <UserLayout/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserLayout/>}/>
+        <Route path="/mycart" element={<h1>My Cart</h1>}/>
+        <Route path="/myorders" element={<h1>My Orders</h1>}/>
+        <Route path="/settings" element={<h1>Settings</h1>}/>
+        <Route path="/profile" element={<h1>My Profile</h1>}/>
+        <Route path="/logout" element={<h1>Logout</h1>}/>
+        <Route path="*" element={<h1>404 Error Page</h1>}/>
+        
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- Book-NPM-App/shoping/client/src/components/Navbar.jsx
+++ Book-NPM-App/shoping/client/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 const Navbar = () => {
   return (
     <div className="navbar">
-      <a>Home</a>
-      <a>My Cart</a>
-      <a>My Order</a>
-      <a>Settings</a>
-      <a>My profile</a>
-      <a>Logout</a>
+      <Link to ="/">Home</Link>
+      <Link to ="/mycart">Cart</Link>
+      <Link to ="/myorders">Order</Link>
+      <Link to ="/settings">Settings</Link>
+      <Link to ="/profile">Profile</Link>
+      <Link to ="/logout">Logout</Link>
     </div>
   )
 }
