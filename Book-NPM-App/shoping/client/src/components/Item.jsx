14310c649d39de90c5605d6006442c7571607b14
Nanu <sureshharshtiwari@gmail.com>
2026-09-10 14:39:39 +0530

    React routes

--- Book-NPM-App/shoping/client/src/components/Item.jsx
+++ Book-NPM-App/shoping/client/src/components/Item.jsx
@@ -3,7 +3,7 @@ import "./Item.css"
 const Item = (props) => {
   return (
     <div className="card">
-      <img src={image} width={100} height={100} alt="Item image"/>
+      <img src={props.images[0]} width={100} height={100} alt="Item image"/>
       <h2>Title:{props.title}</h2>
       <h3>Price: ${props.price}</h3>
       <button>AddCart</button>
